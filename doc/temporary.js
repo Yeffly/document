7782419c50c2ed862e177f5278fc547e553434d6
Yeffly <60810753+Yeffly@users.noreply.github.com>
2020-03-28 18:22:39 +0800

    Update temporary.js

--- doc/temporary.js
+++ doc/temporary.js
@@ -393,16 +393,9 @@ function currentState(){
         })
     }
 }
-document.onkeydown = function(){
-    
+document.onkeydown = function(){  
     if(window.event && window.event.keyCode == 123) {
         event.keyCode=0;
         event.returnValue=false;
-    }
-    if(window.event && window.event.keyCode == 13) {
-        window.event.keyCode = 505;
-    }
-    if(window.event && window.event.keyCode == 8) {
-        window.event.returnValue=false;
     }
 }  
